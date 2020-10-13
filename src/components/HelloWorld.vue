fb3bde898b1f51b81314f5755583fa55d2aa2514
J.Henrique <jhenriqueribeirodesa@gmail.com>
2020-10-12 22:41:41 -0300

    add electron

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,17 @@
   <v-container>
     <v-row class="text-start">
       <v-col>
-      <h1>Hello world</h1>
+      <h1>Cadastro de Produto</h1>
       <v-form>
-        <v-text-field label="testando">
-        </v-text-field>
+        <v-text-field
+          label="Descrição"
+          v-model="product.description"
+        />
+        <v-btn
+          @click="cadastrar"
+          color="primary"
+        >Cadastrar</v-btn>
+
       </v-form>
       </v-col>
     </v-row>
@@ -18,57 +25,20 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'HelloWorld',
 
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader'
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify'
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+  data () {
+    return ({
+      product: {
+        description: ''
       }
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com'
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com'
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify'
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs'
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify'
-      }
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer'
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/getting-started/pre-made-layouts'
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-      }
-    ]
-  })
+    })
+  },
+
+  methods: {
+    cadastrar () {
+      console.log('Cadastrando')
+      console.log(this.product.description)
+      // console.log(__dirname)
+    }
+  }
 })
 </script>
