5e8568360c5540d8240d1bdca95a4b53ca8734d1
J.Henrique <jhenriqueribeirodesa@gmail.com>
2020-10-31 18:55:07 -0300

    adjusts focus create cliente

--- src/views/Clientes/CreateClientes.vue
+++ src/views/Clientes/CreateClientes.vue
@@ -2,15 +2,15 @@
 <v-container>
   <h1>Cadastro de Clientes</h1>
   <v-form ref="form">
-    <v-text-field label="Nome" v-model="cliente.name"/>
+    <v-text-field autofocus label="Nome" v-model="cliente.name"/>
     <v-text-field label="Apelido" v-model="cliente.shortName" />
     <v-text-field label="CPF" v-model="cliente.cpf" />
     <v-text-field label="Endereço" v-model="cliente.address" />
     <v-text-field label="Telefone" v-model="cliente.phone" />
 
     <v-row justify="space-around">
-      <v-btn color="primary" @click="cadastrar">Salvar</v-btn>
-      <v-btn to="/clientes">Cancelar</v-btn>
+      <v-btn tabindex="6" to="/clientes">Cancelar</v-btn>
+      <v-btn tabindex="5" color="primary" @click="cadastrar">Salvar</v-btn>
     </v-row>
   </v-form>
   <v-dialog
@@ -22,7 +22,7 @@
       <v-card-title>Sucesso</v-card-title>
       <v-card-text>Cliente Cadastrado com sucesso</v-card-text>
       <v-card-actions class="justify-center">
-        <v-btn color="primary" @click="showMessage = false">OK</v-btn>
+        <v-btn ref="btn_ok" color="primary" @click="showMessage = false">OK</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -53,6 +53,9 @@ export default Vue.extend({
       const form = this.$refs.form as HTMLFormElement
       form.reset()
       this.showMessage = true
+      setTimeout(() => {
+        (this.$refs.btn_ok as unknown as {$el: HTMLButtonElement}).$el.focus()
+      }, 0)
     }
   }
 })
