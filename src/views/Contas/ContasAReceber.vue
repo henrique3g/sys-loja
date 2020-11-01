53fca1a7ea64ac2c6b4e4b9cf41837d491dfdf04
J.Henrique <jhenriqueribeirodesa@gmail.com>
2020-11-01 15:20:23 -0300

    parse contas a receber to class component, add select parcelas, show total, total vencido, total selecionado

--- src/views/Contas/ContasAReceber.vue
+++ src/views/Contas/ContasAReceber.vue
@@ -10,12 +10,28 @@
       v-model="selectedCliente"
       :clearable="true"
     />
+    <v-row class="mb-3" no-gutters>
+      <v-card class="mr-3 white--text" color="primary">
+        <v-card-title>Total em aberto</v-card-title>
+        <v-card-text class="white--text font-weight-bold">{{total | currency}}</v-card-text>
+      </v-card>
+      <v-card class="white--text" color="error">
+        <v-card-title>Total vencido</v-card-title>
+        <v-card-text class="white--text font-weight-bold">{{totalVencido | currency}}</v-card-text>
+      </v-card>
+      <v-card class="ml-3 white--text" color="success">
+        <v-card-title>Total selecionado</v-card-title>
+        <v-card-text class="white--text font-weight-bold">{{totalSelecionado | currency}}</v-card-text>
+      </v-card>
+    </v-row>
     <v-data-table
       :disable-sort="true"
       :headers="tableHeaders"
       :items="contas"
       :disable-pagination="true"
       :hide-default-footer="true"
+      show-select
+      v-model="selectedContas"
     >
       <template v-slot:[`item.dueDate`]="{ item }">
         <div :class="vencida(item.dueDate) ? 'red--text' : ''">
@@ -48,43 +64,53 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Vue, Component, Watch } from 'vue-property-decorator'
 import { format, compareAsc, isBefore } from 'date-fns'
 import { Cliente } from '@/models/Cliente'
 import { ClienteService } from '@/services/ClienteService'
 import { DataTableHeader } from 'vuetify'
 import { Parcela } from '@/models/Parcela'
 
-export default Vue.extend({
-  data: () => ({
-    clientes: [] as Cliente[],
-    contas: [] as Parcela[],
-    selectedCliente: null as Cliente | null
-  }),
+@Component
+export default class ContasAReceber extends Vue {
+  clientes = [] as Cliente[];
+  contas = [] as Parcela[];
+  selectedCliente = null as Cliente | null;
+  selectedContas = [] as Parcela[]
   async mounted () {
     this.clientes = await ClienteService.findAll()
-  },
-  methods: {
-    formatDate (date: Date) {
-      return format(date, 'dd/MM/y')
-    },
-    vencida (date: Date) {
-      return isBefore(new Date(format(date, 'MM-dd-y')), new Date(format(new Date(), 'MM-dd-y')))
-    }
-  },
-  watch: {
-    selectedCliente: async function (clienteId: number) {
-      console.log('buscando...')
-      if (!clienteId) return
-      const contas = await ClienteService.getContas(clienteId)
-      this.contas = contas.sort((item1, item2) => compareAsc(item1.dueDate, item2.dueDate))
-    }
-  },
-  computed: {
-    total () {
-      return this.contas.reduce((prev, curr) => prev + curr.value - curr.valueReceived, 0)
-    },
-    tableHeaders: (): DataTableHeader[] => [
+  }
+
+  formatDate (date: Date) {
+    return format(date, 'dd/MM/y')
+  }
+
+  vencida (date: Date): boolean {
+    return isBefore(new Date(format(date, 'MM-dd-y')), new Date(format(new Date(), 'MM-dd-y')))
+  }
+
+  @Watch('selectedCliente')
+  async watchselectedCliente (clienteId: number) {
+    console.log('buscando...')
+    if (!clienteId) return
+    const contas = await ClienteService.getContas(clienteId)
+    this.contas = contas.sort((item1, item2) => compareAsc(item1.dueDate, item2.dueDate))
+  }
+
+  get total () {
+    return this.contas.reduce((prev, curr) => prev + curr.value - curr.valueReceived, 0)
+  }
+
+  get totalVencido () {
+    return this.contas.reduce((prev, curr) => this.vencida(curr.dueDate) ? curr.value + prev : prev, 0)
+  }
+
+  get totalSelecionado () {
+    return this.selectedContas.reduce((prev, curr) => prev + curr.value, 0)
+  }
+
+  get tableHeaders (): DataTableHeader[] {
+    return [
       {
         text: 'Venda N°',
         value: 'contaAReceber.venda.id',
@@ -115,8 +141,7 @@ export default Vue.extend({
         text: 'Restante',
         value: 'restante'
       }
-
     ]
   }
-})
+}
 </script>
